24c7ad83102b8c4d3548e5f46fcbcf20eb8e26ba
AmanSDET <52653529+AmanSDET@users.noreply.github.com>
2024-09-20 21:36:27 +0530

    Fourthupdated

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -7,7 +7,7 @@ import {BsGithub} from "react-icons/bs"
 const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
-        <li onClick={handleNav} ><Link  to="//twitter.com/_pappy_jay_" target='_blank' className='nav-link'><FaTwitter /></Link></li>
+        <li onClick={handleNav} ><Link  to="//twitter.com/Aman231501" target='_blank' className='nav-link'><FaTwitter /></Link></li>
         <li onClick={handleNav} ><Link  to="//www.linkedin.com/in/aman-chouhan-2301" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
         <li onClick={handleNav} ><Link  to="//github.com/AmanSDET" target='_blank' className='nav-link'><BsGithub /></Link></li>
         <li onClick={handleNav} ><a  href="mailto:amanchouhan2801@gmail.com" target='_blank' className='nav-link' rel="noreferrer"><BiEnvelope /></a></li>
